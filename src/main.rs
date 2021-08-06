c01c1cb234878bb1fff2c2dcce4976977487a175
Xyberista <50119873+Xyberista@users.noreply.github.com>
2021-08-06 17:24:23 -0500

    basic movement and setup for other renderables

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,50 @@
 use bracket_lib::prelude::*;
+use components::{Name, Position, Renderable};
+use map::Map;
+use player::Player;
+use specs::prelude::*;
 
 mod game;
-pub use game::State;
+pub use game::*;
+mod components;
+mod player;
+mod map;
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Fabriconth")
         .with_fps_cap(60.0)
-        .with_fullscreen(true)
         .build()?;
         
-    let gamestate = State {
-
+    let mut gs = State {
+        ecs: World::new(),
+        runstate: RunState::Running,
     };
+
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<Player>();
+    gs.ecs.register::<Name>();
     
-    main_loop(context, gamestate)
+    let map: Map = Map::new_base_map();
+    let (player_x, player_y) = (0, 0);
+
+    gs.ecs.create_entity()
+        .with(Position {
+            x: player_x,
+            y: player_y,
+        })
+        .with(Renderable {
+            glyph: to_cp437('@'),
+            fg: RGB::named(YELLOW),
+            bg: RGB::named(BLACK),
+        })
+        .with(Player {})
+        .with(Name {name: "Player".to_string() })
+        .build();
+    
+    gs.ecs.insert(map);
+    gs.ecs.insert(Point::new(player_x, player_y));
+
+    main_loop(context, gs)
 }
