9c5479dce58721634ed7147db03c169cb6ed2bb3
Xyberista <50119873+Xyberista@users.noreply.github.com>
2021-08-06 14:32:33 -0500

    basic hello world example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+mod game;
+pub use game::State;
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Fabriconth")
+        .with_fps_cap(60.0)
+        .with_fullscreen(true)
+        .build()?;
+        
+    let gamestate = State {
+
+    };
+    
+    main_loop(context, gamestate)
 }
